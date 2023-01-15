70281bb7f88948686d29f030180c9168d4eedcbc
feiwhang <feiwhang@gmail.com>
2023-01-15 16:19:39 +0700

    feat: create models

---
+++ app/db.py
@@ -0,0 +1,41 @@
+from typing import Optional, List
+from .config import settings
+
+import databases
+import ormar
+import sqlalchemy
+
+database = databases.Database(settings.db_url)
+metadata = sqlalchemy.MetaData()
+
+
+class BaseMeta(ormar.ModelMeta):
+    metadata = metadata
+    database = database
+
+
+class Product(ormar.Model):
+    class Meta():
+        tablename = 'products'
+        metadata = metadata
+        database = database
+
+    id: int = ormar.Integer(primary_key=True)
+    name: str = ormar.String(max_length=100, unique=True, nullable=False)
+    stock: int = ormar.Integer(nullable=False)
+
+
+class VendingMachine(ormar.Model):
+    class Meta():
+        tablename = 'vending_machines'
+        metadata = metadata
+        database = database
+
+    id: int = ormar.Integer(primary_key=True)
+    name: str = ormar.String(max_length=100, unique=True, nullable=False)
+    location: str = ormar.String(max_length=100, nullable=False)
+    products: Optional[List[Product]] = ormar.ManyToMany(Product)
+
+
+engine = sqlalchemy.create_engine(settings.db_url)
+metadata.create_all(engine)
--- app/main.py
+++ app/main.py
@@ -1,8 +1,23 @@
 from fastapi import FastAPI
+from app.db import database, VendingMachine
 
 app = FastAPI(title="MUIC Vending Machine")
 
 
 @app.get("/")
-def read_root():
-    return {"main": "app"}
+async def read_root():
+    return await VendingMachine.objects.all()
+
+
+@app.on_event("startup")
+async def startup():
+    if not database.is_connected:
+        await database.connect()
+    # create a dummy entry
+    await VendingMachine.objects.get_or_create(name="Soda Zaaa", location="Old building 4th floor")
+
+
+@app.on_event("shutdown")
+async def shutdown():
+    if database.is_connected:
+        await database.disconnect()
