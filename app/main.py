26b3ce4022764edc53c2497c76d8c9e8d1b30138
feiwhang <feiwhang@gmail.com>
2023-01-15 17:11:24 +0700

    feat: add endpoint for creating vending machine

--- app/main.py
+++ app/main.py
@@ -19,3 +19,17 @@ async def startup():
 async def shutdown():
     if database.is_connected:
         await database.disconnect()
+
+
+@app.post("/vending_machine/create")
+async def create_vending_machine(name: str, location: str):
+    """
+    Creates a new vending machine by taking a name and location as input, then saves it to the database.
+    An error message is returned if the vending machine name already exists or if an exception occurs during the save operation.
+    """
+    new_machine = VendingMachine(name=name, location=location)
+    try:
+        await new_machine.save()
+    except Exception as e:
+        return {"error_message": e.message}
+    return new_machine
